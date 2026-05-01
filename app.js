968ae27b95c0d37a6655dd828bf63eb658bbff26
cruffing0528 <cruffing0528@sdsu.edu>
2026-05-01 14:05:59 -0700

    initial setup module 15

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import 'dotenv/config';
+import routes from './routes/routes.js';
+import connectDB from './db/connect.js';
+import bodyParser from 'body-parser';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const app = express();
-const PORT = 5000;
+const app = express()
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+// Loads index.html from the public folder when the root URL is accessed
+app.use('/', routes)
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://127.0.0.1:${PORT}`);
-});
+const init = async () => {
+    try {
+        await connectDB(process.env.DB);
+        console.log("✅ Connected to MongoDB database");
+        app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+    }
+    catch (err) {
+        console.error("❌ Connection error:", err.message);
+    }
+}
+
+init();
---
+++ routes/routes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import {getAllEmployees, getEmployee, createEmployee, updateEmployee, deleteEmployee} from '../controllers/employees.js';
+
+const router = express.Router()
+
+router.route('/api/employees')
+    .get(getAllEmployees)
+//     .post(createEmployee);
+
+// router.route('/:id')
+//     .get(getEmployee)
+//     .patch(updateEmployee)
+//     .delete(deleteEmployee);
+
+export default router
